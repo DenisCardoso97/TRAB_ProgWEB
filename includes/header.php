fca3d5007f2ca09d4a0af4821b08839e2311f7af
Vinicius Orejana Valente <viniciusorejana@yahoo.com.br>
2019-10-17 17:02:04 -0400

    coisas

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" type="text/css" href="css/header.css">
+<head>
+		<meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	    <link rel="stylesheet" href="css/login.css">
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+	    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<header>
+	<div>
+		
+	</div>
+	<div id="barra1">
+		<div id= "loginECadastro">
+			<button onclick="window.location.href='login.html'" id="btn">Login</button>
+			<button onclick="window.location.href='cadastrar.html'" id="btn">Cadastrar</button>
+		</div>
+	</div>
+	<div id="barra2">
+		<a id="imagem"><img src="https://cdn.icon-icons.com/icons2/67/PNG/512/plane_takeoff_13263.png">
+		<button onclick="window.location.href='voos.php'" id="btn">Voos e destinos</button>
+		<button onclick="window.location.href='admnistrar.php'" id="btn">Administrar voos</button>
+		<button onclick="window.location.href='informações.php'" id="btn">Informações de viagem</button>
+		<button onclick="window.location.href='avioesDoForro.php'" id="btn">Aviões do Forrô</button>
+	</div>
+</header>
--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Titulo</title>
-</head>
-	<body>
-		Hello World!
-	</body>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	    <title>Companhia Aérea - Aviões do Forró</title>
+	    <link rel="stylesheet" href="css/login.css">
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+	    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	    <style type="text/css">
+	    	#diva {
+				margin-right: auto;
+		    	margin-left: 28%;
+			}
+	    </style>
+	</head>
+		<body>
+			<div>
+				<?php include("includes/header.php"); ?>	
+			</div>
+		    <div id="diva">
+		    	<h1>Bem vindo ao Aviões do Forró!</h1>
+		    </div>
+		</body>
 </html>
