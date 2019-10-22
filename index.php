2e6d442907abc329cb505a5a8bb3e99142ff4fed
Lucas Avanzi <lucasavanzi97@gmail.com>
2019-10-21 22:11:21 -0400

    Alterações realizadas durante a aula de progweb dia 21/10

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
 			</div>
 			<link rel="stylesheet" type="text/css" href="css/slideshow.css">
 			<div>
-				<img src="images/img1.jpg" class="mySlides img-fluid">
-				<img src="images/img2.jpg" class="mySlides img-fluid">
-				<img src="images/img3.jpg" class="mySlides img-fluid">
+				<img src="images/img1.jpg" class="mySlides img-fluid" style="width: 100%;" >
+				<img src="images/img2.jpg" class="mySlides img-fluid" style="width: 100%;">
+				<img src="images/img3.jpg" class="mySlides img-fluid" style="width: 100%;">
 			</div>
 			<?php include("includes/script.php"); ?>
 		</body>
---
+++ sobre.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<?php include("includes/head.php"); ?>
+<head>
+	<title>Voos e destinos</title>
+</head>
+<body>
+	<div >
+        <?php include("includes/header.php"); ?>  
+        <div class="container">
+            <h1>SOBRE</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit sollicitudin nibh non dictum. Vivamus eros tortor, dictum id justo eget, imperdiet aliquam nisl. Cras ultrices ac nulla quis scelerisque. Vivamus elit magna, dignissim in neque sed, porttitor auctor urna. Etiam sed urna euismod, dignissim diam sit amet, sagittis mi. Ut suscipit diam ipsum, id bibendum magna finibus a. Integer eu diam id tortor rutrum maximus. Donec justo elit, tristique nec turpis in, mattis dictum metus. Donec vel cursus orci. Phasellus ultricies dapibus velit id malesuada. Suspendisse potenti.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit sollicitudin nibh non dictum. Vivamus eros tortor, dictum id justo eget, imperdiet aliquam nisl. Cras ultrices ac nulla quis scelerisque. Vivamus elit magna, dignissim in neque sed, porttitor auctor urna. Etiam sed urna euismod, dignissim diam sit amet, sagittis mi. Ut suscipit diam ipsum, id bibendum magna finibus a. Integer eu diam id tortor rutrum maximus. Donec justo elit, tristique nec turpis in, mattis dictum metus. Donec vel cursus orci. Phasellus ultricies dapibus velit id malesuada. Suspendisse potenti.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit sollicitudin nibh non dictum. Vivamus eros tortor, dictum id justo eget, imperdiet aliquam nisl. Cras ultrices ac nulla quis scelerisque. Vivamus elit magna, dignissim in neque sed, porttitor auctor urna. Etiam sed urna euismod, dignissim diam sit amet, sagittis mi. Ut suscipit diam ipsum, id bibendum magna finibus a. Integer eu diam id tortor rutrum maximus. Donec justo elit, tristique nec turpis in, mattis dictum metus. Donec vel cursus orci. Phasellus ultricies dapibus velit id malesuada. Suspendisse potenti.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit sollicitudin nibh non dictum. Vivamus eros tortor, dictum id justo eget, imperdiet aliquam nisl. Cras ultrices ac nulla quis scelerisque. Vivamus elit magna, dignissim in neque sed, porttitor auctor urna. Etiam sed urna euismod, dignissim diam sit amet, sagittis mi. Ut suscipit diam ipsum, id bibendum magna finibus a. Integer eu diam id tortor rutrum maximus. Donec justo elit, tristique nec turpis in, mattis dictum metus. Donec vel cursus orci. Phasellus ultricies dapibus velit id malesuada. Suspendisse potenti.
+            </p>
+            <br><br><br>
+            <footer>
+                <span>TODOS OS DIREITOS RESERVADOS.</span>
+            </footer>
+        </div>
+
+        
+    </div>
+</body>
+</html>
