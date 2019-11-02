10de902428f50cee80a010cf996d679926ad3f1f
Vinicius Orejana Valente <viniciusorejana@yahoo.com.br>
2019-11-02 14:52:19 -0400

    passagens.php

--- passagens.php
+++ passagens.php
@@ -14,16 +14,60 @@
 	<div class="container">      
         <h1>Passagens</h1>
         <div>
-            <form>     
+            <form style="height: 700px;" class="align-items-center">
+                <div>
+                    
+                </div>
+                <div class="form-check-inline mt-3">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="opcao1" checked>
+                    <label class="form-check-label" for="inlineRadio1">Ida e Volta</label>
+                </div>
+                <div class="form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="opcao2">
+                    <label class="form-check-label" for="inlineRadio2">Só Ida</label>
+                </div>
+                <div class="form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="opcao3">
+                    <label class="form-check-label" for="inlineRadio2">Multidestino</label>
+                </div>
+                <div class="form-row">
+                    <div class="form-group mr-1 mb-2 mt-4">
+                        <label for="staticEmail2">Origem</label>
+                        <input type="text" class="form-control" id="staticEmail2" >
+                    </div>
+                    <div class="form-group mb-2 mt-4">
+                        <label for="staticEmail2">Destino</label>
+                        <input type="text" class="form-control" id="staticEmail2" >
+                    </div>
+                    <div class="form-group col-1 ml-3 mb-2 mt-4">
+                        <label for="staticEmail2">Datas</label>
+                        <input type="date" class="form-control" id="staticEmail2" >
+                    </div>
+                    <div class="form-group col-1 mb-2 mt-auto">
+                        <input type="date" class="form-control" id="staticEmail2" >
+                    </div>
+                    <div class="form-group ml-3 mb-2 mt-auto">
+                        <label for="inputEstado">Passageiros e Classe</label>
+                        <select id="inputEstado" class="form-control">
+                            <option selected>Escolher...</option>
+                            <option>...</option>
+                        </select>
+                    </div>
+                    <div class="form-group ml-3 mb-2 mt-auto">
+                        <button type="submit" class="btn btn-primary">Procurar</button>
+                    </div>
+                </div>
+                <div class="form-group form-row">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="gridCheck">
+                        <label class="form-check-label" for="gridCheck">
+                            Não defini as datas
+                        </label>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <!-- Footer -->
     <?php include("includes/footer.php") ?>
 </body>
--- sobre.php
+++ sobre.php
@@ -12,7 +12,7 @@
     </div>
 
     <div class="container">
-        <h1>SOBRE</h1>
+        <h1>SOBRE - Aviões do Forró</h1><br>
          <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit sollicitudin nibh non dictum. Vivamus eros tortor, dictum id justo eget, imperdiet aliquam nisl. Cras ultrices ac nulla quis scelerisque. Vivamus elit magna, dignissim in neque sed, porttitor auctor urna. Etiam sed urna euismod, dignissim diam sit amet, sagittis mi. Ut suscipit diam ipsum, id bibendum magna finibus a. Integer eu diam id tortor rutrum maximus. Donec justo elit, tristique nec turpis in, mattis dictum metus. Donec vel cursus orci. Phasellus ultricies dapibus velit id malesuada. Suspendisse potenti.
         </p>
